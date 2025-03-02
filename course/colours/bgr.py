3eeb875cdaa2fc72e87642cc62f938504375eb21
mriansyah93 <mriansyah93@gmail.com>
2025-03-02 14:26:04 +0800

    add bgr and gray course

--- course/colours/bgr.py
+++ course/colours/bgr.py
@@ -1,6 +1,6 @@
 import cv2
 citra = cv2.imread('sample.png')
-cv2.imshow('hmbtn', citra)
+cv2.imshow('hmbtn bgr', citra)
 
 # array 3 dimensi (631, 617, 3) = N x M x b
 print(citra.shape)
@@ -10,13 +10,19 @@ blue_layer = citra[:,:,0]
 print(blue_layer)
 # array 2 dimensi (631, 617)  untuk layer biru saja
 print(blue_layer.shape)
+# blue layer saja = 8 bit
+cv2.imshow("blue layer", blue_layer)
 
 # green 
+# green layer saja = 8 bit
 green_layer = citra[:,:,1]
 print(green_layer)
+cv2.imshow("green layer", green_layer)
 
 # red 
 red_layer = citra[:,:,2]
 print(red_layer)
+cv2.imshow("red layer", red_layer)
+
 
 cv2.waitKey()
