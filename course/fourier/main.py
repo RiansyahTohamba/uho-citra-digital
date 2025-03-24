547551929847d8151f633b9b197d42bcdbc7236b
mriansyah93 <mriansyah93@gmail.com>
2025-03-24 15:52:42 +0800

    measure the size

--- course/fourier/main.py
+++ course/fourier/main.py
@@ -1,9 +1,17 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 # Baca gambar dalam mode grayscale
-image = cv2.imread('sample.png', cv2.IMREAD_GRAYSCALE)
+image_path = 'sample.png'
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Ukuran asli gambar dalam byte
+original_size = os.path.getsize(image_path)
+
+# Ukuran asli gambar dalam piksel
+original_shape = image.shape
 
 # Transformasi Fourier
 dft = np.fft.fft2(image)
@@ -12,14 +20,29 @@ dft_shift = np.fft.fftshift(dft)
 # Magnitude Spectrum
 magnitude_spectrum = 20 * np.log(np.abs(dft_shift) + 1)  # +1 untuk menghindari log(0)
 
-# Normalisasi ke rentang 0-255 agar bisa ditampilkan dengan cv2.imshow
+# Normalisasi agar bisa disimpan sebagai gambar
 magnitude_spectrum = cv2.normalize(magnitude_spectrum, None, 0, 255, cv2.NORM_MINMAX)
 magnitude_spectrum = np.uint8(magnitude_spectrum)
 
-# Tampilkan hasil menggunakan OpenCV
-cv2.imshow("Gambar Asli", image)
-cv2.imshow("Magnitude Spectrum", magnitude_spectrum)
+# Simpan gambar hasil transformasi Fourier sementara
+fourier_image_path = 'fourier_output.png'
+cv2.imwrite(fourier_image_path, magnitude_spectrum)
 
-# Tunggu input dari user lalu tutup semua jendela
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Ukuran setelah transformasi dalam byte
+transformed_size = os.path.getsize(fourier_image_path)
+
+# Ukuran setelah transformasi dalam piksel
+transformed_shape = magnitude_spectrum.shape
+
+# Tampilkan gambar asli dan hasil transformasi
+fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+axes[0].imshow(image, cmap='gray')
+axes[0].set_title(f'Gambar Asli (Size: {original_shape}, {original_size} bytes)')
+axes[0].axis('off')
+
+axes[1].imshow(magnitude_spectrum, cmap='gray')
+axes[1].set_title(f'Magnitude Spectrum (Size: {transformed_shape}, {transformed_size} bytes)')
+axes[1].axis('off')
+
+plt.show()
